260353f92ade68d470a655b58afa9119e02c2c34
unknown <cm@nikpm.com>
2020-08-17 17:24:52 -0500

    first commit

---
+++ src/main/java/com/cmk/trygit/TrygitApplication.java
@@ -0,0 +1,13 @@
+package com.cmk.trygit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrygitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrygitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cmk/trygit/TrygitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cmk.trygit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrygitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
